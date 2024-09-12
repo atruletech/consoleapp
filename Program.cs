5faabd77c81adbb23104f493ac5b7b28c51711c2
mehran-AtRule <mehran.hanif@atrule.com>
2024-09-12 22:14:32 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var hit = 0;
+do
+{
+    hit++;
+    Console.WriteLine(DateTime.UtcNow);
+    Thread.Sleep(1000);
+    
+}while (hit <= 2);
